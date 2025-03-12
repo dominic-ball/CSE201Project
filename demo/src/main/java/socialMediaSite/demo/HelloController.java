46e53ba5abb255fedf61c136dcb514ac43fe41a2
carterbuchholz <cbuchholz6@gmail.com>
2025-03-12 14:32:34 -0400

    Added Say Hello Class
    Added a class that displays a hello message on the website

---
+++ demo/src/main/java/socialMediaSite/demo/HelloController.java
@@ -0,0 +1,15 @@
+package socialMediaSite.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Welcome to gaming social media";
+    }
+}
