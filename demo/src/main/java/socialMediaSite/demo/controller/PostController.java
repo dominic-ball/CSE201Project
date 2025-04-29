5988e81fe2a3ca5d1f8190c32bc5a5ed395a57a2
dominic-ball <dominicjeromeball@gmail.com>
2025-04-28 22:15:39 -0400

    added likes and comments to posts

--- demo/src/main/java/socialMediaSite/demo/controller/PostController.java
+++ demo/src/main/java/socialMediaSite/demo/controller/PostController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import socialMediaSite.demo.model.Post;
 import socialMediaSite.demo.repository.PostRepository; 
@@ -31,5 +32,31 @@ public class PostController {
         } else {
             return "redirect:/profile"; 
         }
+    }
+
+    @PostMapping("/posts/{id}/like")
+    public String likePost(@PathVariable Long id) {
+        Optional<Post> optionalPost = postRepository.findById(id);
+
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            post.setLikes(post.getLikes() + 1);
+            postRepository.save(post);
+        }
+
+        return "redirect:/posts/" + id; 
+    }
+
+    @PostMapping("/posts/{id}/comment")
+    public String commentOnPost(@PathVariable Long id, String comment) {
+        Optional<Post> optionalPost = postRepository.findById(id);
+
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            post.getComments().add(comment);
+            postRepository.save(post);
+        }
+
+        return "redirect:/posts/" + id; 
     }
 }
